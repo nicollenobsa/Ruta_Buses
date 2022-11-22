d33a80dfdac018c003ba7c3635215ad39c565afe
nicollenobsa <nicollenobsa@gmail.com>
2022-11-22 16:49:44 -0500

    Commit Uno

---
+++ src/main/java/com/nicolle/Bus/ControlbusesApplication.java
@@ -0,0 +1,13 @@
+package com.nicolle.Bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlbusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlbusesApplication.class, args);
+	}
+
+}
